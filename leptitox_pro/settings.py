b213ea6118bd48182d17609e8152d4b5e7b688cc
unknown <moi.basir@gmail.com>
2020-02-11 02:03:35 +0500

    Deleted email settings

--- leptitox_pro/settings.py
+++ leptitox_pro/settings.py
@@ -147,15 +147,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-
-
 TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': False,
