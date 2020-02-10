cbb1bf9fe687885d1cfca4235c127334b58768ef
unknown <moi.basir@gmail.com>
2020-02-11 01:30:10 +0500

    Database Setup

--- leptitox_pro/settings.py
+++ leptitox_pro/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'leptitox_pro.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'leptitox',
+        'NAME': 'leptitox_proj',
         'USER': 'postgres',
         'PASSWORD': 'johndoe',
         'PORT': 5432,  # ! not in quotes
